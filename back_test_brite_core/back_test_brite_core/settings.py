8aa5af907b1b6758905bfd7bb0d0b526c71fa336
Bogdan <b.sinchuk@itcg.ua>
2018-11-07 18:24:40 +0200

    django-cors-headers configuration

--- back_test_brite_core/back_test_brite_core/settings.py
+++ back_test_brite_core/back_test_brite_core/settings.py
@@ -38,12 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third-party
+    'corsheaders',
+
+    # local
     'risks',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if DEBUG:
+    CORS_ORIGIN_ALLOW_ALL = True
+else:
+    CORS_ORIGIN_WHITELIST = (
+        # 'google.com',
+        # 'hostname.example.com',
+        'localhost:8000',
+        '127.0.0.1:9000'
+    )
