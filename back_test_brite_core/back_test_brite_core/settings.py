d8b5f716ac6090087e8310977ed84ea1b2c7c610
Bohdan Sinchuk <sinchuk140995@gmail.com>
2018-11-21 03:44:03 +0200

    install whitenoise package; add static files congiguration

--- back_test_brite_core/back_test_brite_core/settings.py
+++ back_test_brite_core/back_test_brite_core/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 if DEBUG:
     CORS_ORIGIN_ALLOW_ALL = True
