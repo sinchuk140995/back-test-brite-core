9444298769446e163b139bbbd868fee47db1567f
Bogdan <b.sinchuk@itcg.ua>
2018-11-22 19:09:41 +0200

    add database_diagram.png; delete comments in risks/models.py

--- risks/models.py
+++ risks/models.py
@@ -29,11 +29,7 @@ class Field(models.Model):
         verbose_name=_('insurance risk'),
         related_name='fields',
     )
-    # field_type = models.PositiveSmallIntegerField(_('type'), choices=TYPE_CHOICES)
     field_type = models.CharField(_('type'), max_length=10, choices=TYPE_CHOICES)
-
-    # class Meta:
-    #     ordering = ('name',)
 
     def __str__(self):
         return self.name
