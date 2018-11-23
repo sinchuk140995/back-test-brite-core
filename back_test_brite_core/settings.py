bdb644be3437128c55cb9f2388bde03b29cf5e83
Bogdan <b.sinchuk@itcg.ua>
2018-11-23 13:40:05 +0200

    delete comments from main urls.py; settings.py: change USE_I18N to false, delete django.contrib.messages

--- back_test_brite_core/settings.py
+++ back_test_brite_core/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    # 'django.contrib.messages',
     'django.contrib.staticfiles',
 
     # third-party
@@ -129,7 +128,7 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
-# USE_I18N = True
+USE_I18N = False
 
 USE_L10N = True
 
--- back_test_brite_core/urls.py
+++ back_test_brite_core/urls.py
@@ -15,18 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.decorators.csrf import csrf_exempt
-
-from risks import views
 
 
 urlpatterns = [
     url(r'^api/client/', include('clients.urls')),
     url(r'^api/', include('risks.urls')),
-
-    # url(r'^api/risk/create/$', csrf_exempt(views.InsuranceRiskCreateView.as_view()), name='insurance-risk-create'),
-    # url(r'^api/client/risk/$', views.ClientInsuranceRiskListView.as_view(), name='client-insurance-risks'),
-    # url(r'^api/client/risk/(?P<pk>\d+)/$', csrf_exempt(views.InsuranceRiskTakeView.as_view()), name='insurance-risk-take'),
-    # url(r'^api/client/risk/(?P<pk>\d+)/edit/$', csrf_exempt(views.InsuranceRiskEditView.as_view()), name='insurance-risk-edit'),
     url(r'^admin/', admin.site.urls),
 ]
